3c94759f511aff582d30e70127cc64baad22db3c
MJ102 <mj102@MJ102-no-MacBook-Air.local>
2016-04-18 21:35:36 +0900

    modified

--- osaki/chapter02/knock17.py
+++ osaki/chapter02/knock17.py
@@ -1,15 +1,3 @@
-#'''
-#python knock017.py col1.txt
-#'''
-#import sys
-#
-#d = dict()
-#for line in open(sys.argv[1]):
-#    d[line.strip()] = 0
-#
-#for key in d.keys():
-#    print key
-
 f=open("col1.txt","r")
 s=f.read()
 f.close()
