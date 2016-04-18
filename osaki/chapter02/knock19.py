caab249d919ed0a0c09be8313964eca08ad34d2a
MJ102 <mj102@MJ102-no-MacBook-Air.local>
2016-04-19 00:57:05 +0900

    add

---
+++ osaki/chapter02/knock19.py
@@ -0,0 +1,17 @@
+f=open("col1.txt","r")
+s=f.read()
+f.close()
+d=dict()
+t1=[]
+t2=[]
+for i in s.split("\n"):
+    if d.has_key(i)==False:
+        d[i]=0
+    else:
+       d[i]=str(int(d[i])+1)
+for i in d.keys():
+    t1=t1+[[i,d[i]]]
+t2=sorted(t1,key=lambda x:x[1])
+t2.reverse()
+for i in t2:
+    print i[0]
