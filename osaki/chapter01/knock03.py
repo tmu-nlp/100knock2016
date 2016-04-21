1fd3982fdaa97cfa808416c2971527d26adb8351
osaki <osaki@osaki.local>
2016-04-21 16:10:36 +0900

    modified

--- osaki/chapter01/knock03.py
+++ osaki/chapter01/knock03.py
@@ -1,17 +1,8 @@
 s="Now I need a drink,alcoholic of course,after the heavy lectures involving quantum mechanics."
-t=""
 c=[]
-for i in range(len(s)):
-    if s[i]==" ":
-        c=c+[len(t)]
-        t=""
-    elif s[i]==".":
-        c=c+[len(t)]
-        t=""
-    elif s[i]==",":
-        c=c+[len(t)]
-        t=""
-    else:
-        t=t+s[i]
+s=s.replace(","," ")
+s=s.replace("."," ")
+for i in s.split():
+    c=c+[len(i)]
 
 print(c)
