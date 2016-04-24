138eda17686bdbe919ff5c9ec5c4389265cce928
尾形朋哉 <tomoya@flamie.local>
2016-04-24 20:24:28 +0900

    upload

---
+++ tomoya/chapter03/knock28.py
@@ -0,0 +1,16 @@
+#coding: utf-8
+import sys
+import re
+f_in = open("UK.txt", "r").read()
+repatter1 = re.compile(u"{{基礎情報\s([^\n]*)\n(.*)\n}}", re.DOTALL)
+repatter2 = re.compile("'|({{)|(}})")
+repatter3 = re.compile(r"\[*(.*?)\]*")
+repatter4 = re.compile("\*|\|")
+target = repatter1.findall(f_in)
+if target:
+  temp = {target[0][0]: target[0][1]}
+target = repatter2.sub("", temp["国"])
+target = repatter3.sub(r"\1", target)
+target = repatter4.sub(" ", target)
+print(target)
+
