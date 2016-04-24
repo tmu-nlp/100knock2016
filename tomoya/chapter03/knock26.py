a277a6710aec4ca6aa3ffed7accfb54cc3a32e7e
尾形朋哉 <tomoya@flamie.local>
2016-04-24 19:57:49 +0900

    upgrade

--- tomoya/chapter03/knock26.py
+++ tomoya/chapter03/knock26.py
@@ -2,10 +2,10 @@
 import sys
 import re
 f_in = open("UK.txt", "r").read()
-repatter1 = re.compile("'")
-repatter2 = re.compile(u"{{(基礎情報)(.*)\n}}", re.DOTALL)
-target = repatter1.sub("", f_in)
-target = repatter2.findall(target)
+repatter1 = re.compile(u"{{基礎情報\s([^\n]*)\n(.*)\n}}", re.DOTALL)
+repatter2 = re.compile("'")
+target = repatter1.findall(f_in)
 if target:
   temp = {target[0][0]: target[0][1]}
-print(temp)
+target = repatter2.sub("", temp["国"])
+print(target)
--- tomoya/chapter03/knock27.py
+++ tomoya/chapter03/knock27.py
@@ -2,13 +2,13 @@
 import sys
 import re
 f_in = open("UK.txt", "r").read()
-repatter1 = re.compile("'")
-repatter2 = re.compile(r"\[\[([^:]*?)\]\]")
-repatter3 = re.compile(u"{{(基礎情報)(.*)\n}}", re.DOTALL)
-target = repatter1.sub(u"", f_in)
-target = repatter2.sub(r"\1", target)
-target = repatter3.findall(target)
-print(target)
+repatter1 = re.compile(u"{{基礎情報\s([^\n]*)\n(.*)\n}}", re.DOTALL)
+repatter2 = re.compile("'")
+repatter3 = re.compile(r"\[\[([^:]*?)\]\]")
+target = repatter1.findall(f_in)
 if target:
   temp = {target[0][0]: target[0][1]}
-  print(temp)
+target = repatter2.sub("", temp["国"])
+target = repatter3.sub(r"\1", target)
+print(target)
+
