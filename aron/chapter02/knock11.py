1ec8f46e2114a1d7225eba555dc7c3dc6dfda648
aronsoyol <aronsoyol@gmail.com>
2016-04-28 11:33:50 +0900

    little change

--- aron/chapter02/knock11.py
+++ aron/chapter02/knock11.py
@@ -10,12 +10,22 @@ for line in my_file:
 print ("\n".join(ls))
 
 
+'''
 
+linux command:
 
-# linux command:
+# 1.
+sed -E 's/[        ]+/_/g' hightemp.txt
+
 # not work
-# sed  -E 's/\\t/_/g' hightemp.txt 
-
-# work
+# sed  -E 's/\\t/ /g' hightemp.txt 
 # http://qiita.com/kyanagimoto/items/37cc8e7a242ca2d1a29e
-# sed -E 's/[        ]+/_/g' hightemp.txt
+
+2.
+cat ./hightemp.txt | tr '\t' ' '
+
+3.
+expand -t 1 hightemp.txt
+
+
+'''
