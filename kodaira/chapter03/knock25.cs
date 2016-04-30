c9ae5f3358e3c443108ac39faee94438ba87c84b
KODAIRATOMONORI <kodairatomonori@GN.local>
2016-04-30 21:43:32 +0900

    add comment

--- kodaira/chapter03/knock25.cs
+++ kodaira/chapter03/knock25.cs
@@ -1,6 +1,7 @@
 /*
  * Chapter03, knock25
  * Task: Extract Basic Information and store their
+ * Compile: mcs /target:library /out:***.DLL *.cs
  */
 
 using System;
@@ -12,34 +13,35 @@ using System.Net;
 using System.Text;
 
 
-public class Knock25 {
-    static public void Main(string[] args) {
-        Dictionary<string, string> dict = ExtractTarget(args[0]);
-        foreach (KeyValuePair<string, string> kv in dict) {
-            Console.WriteLine(string.Format("{0}: {1}", kv.Key, kv.Value));
-        }
-            
-    }
-
-    public static Dictionary<string, string> ExtractTarget(string path, string target="{{基礎情報") {
-        bool fi_flag = false;
-        Dictionary<string, string> dict = new Dictionary<string, string>();
-        
-        char []deli = new char []{'\t'};
-        using (StreamReader sr = File.OpenText(path)) {
-            string line = "";
-            while ((line = sr.ReadLine()) != null) {
-                if (fi_flag && line == "}}") fi_flag = false; 
-                if (fi_flag) {
-                    string fi = (System.Text.RegularExpressions.Regex.Replace(line, @"\|(.*) =(.*)$", "$1\t$2"));
-                    string []split = fi.Split(deli);
-                    dict[split[0]] = split[1];
-                }
-                if (System.Text.RegularExpressions.Regex.IsMatch(line, target)) fi_flag = true; 
+namespace BasicInfomationExtraction {
+    public class Knock25 {
+        static public void Main(string[] args) {
+            Dictionary<string, string> dict = ExtractTarget(args[0]);
+            foreach (KeyValuePair<string, string> kv in dict) {
+                Console.WriteLine(string.Format("{0}: {1}", kv.Key, kv.Value));
             }
+                
         }
-        return dict;
 
+        public static Dictionary<string, string> ExtractTarget(string path, string target="{{基礎情報") {
+            bool fi_flag = false;
+            Dictionary<string, string> dict = new Dictionary<string, string>();
+            
+            char []deli = new char []{'\t'};
+            using (StreamReader sr = File.OpenText(path)) {
+                string line = "";
+                while ((line = sr.ReadLine()) != null) {
+                    if (fi_flag && line == "}}") fi_flag = false; 
+                    if (fi_flag) {
+                        string fi = (System.Text.RegularExpressions.Regex.Replace(line, @"\|(.*) =(.*)$", "$1\t$2"));
+                        string []split = fi.Split(deli);
+                        dict[split[0]] = split[1];
+                    }
+                    if (System.Text.RegularExpressions.Regex.IsMatch(line, target)) fi_flag = true; 
+                }
+            }
+            return dict;
+
+        }
     }
 }
-
