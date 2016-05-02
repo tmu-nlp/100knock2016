fafdd83f7eacb3d62297733e77a5be133e72ff73
ryosukee <tmcit.miyazaki@gmail.com>
2016-05-02 12:23:52 +0900

    [add] knock25

---
+++ ryosuke/chapter03/knock25.py
@@ -0,0 +1,24 @@
+from knock20 import getUKtext
+import re
+
+
+def in_base_info(line):
+    if not hasattr(in_base_info, 'bool'):
+        in_base_info.bool = False
+    if line.startswith('{{基礎情報'):
+        in_base_info.bool = True
+    elif line.startswith('}}'):
+        in_base_info.bool = False
+    return in_base_info.bool
+
+
+re_base_info = re.compile('\|(?P<key>.+?) = (?P<value>.+)')
+base_info = dict()
+for line in getUKtext().split('\n'):
+    if in_base_info(line):
+        match = re_base_info.search(line)
+        if match is not None:
+            base_info[match.group('key')] = match.group('value')
+
+for k, v in base_info.items():
+    print(k, ':', v)
