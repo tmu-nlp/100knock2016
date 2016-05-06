90dc69e6e8986fd005635a580bb535c692e1c1ac
aronsoyol <aronsoyol@gmail.com>
2016-05-06 12:16:00 +0900

    25 変更

--- aron/chapter03/knock25.py
+++ aron/chapter03/knock25.py
@@ -1,8 +1,9 @@
 # knock25.py
 import sys;
 import re
-pattern = re.compile(r"^\{基礎情報.*")
-pattern2 = re.compile(r"\|[^\|]*=[^\|]*\|")
+
+pattern = re.compile(r"^\{基礎情報 国\\n\|(.*)\\n}$")
+pattern2 = re.compile(r"^(.*) = (.*)$")
 lst = []
 # num = 0;
 for line in sys.stdin:
@@ -15,12 +16,14 @@ for line in sys.stdin:
 			# print (subStr)
 			m = pattern.match(subStr)
 			if(m):
-				print (m.group(0))
-				m2 = re.findall(pattern2, m.group(0))
-				# print (m2)
-				print (len(m2))
-				for item in m2:
-					print (item)
+				# print (m.group(1))
+				lst2 = m.group(1).split(r"\n|")
+				for item in lst2:
+					m2 = re.findall(pattern2, item)
+					# print (item)
+					# print (len(m2))
+					for item2 in m2:
+						print ("%s =|= %s" % (item2[0], item2[1]))
 	if(len(lst) == 0):
 		print ("finish")
 	else:
