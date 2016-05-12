7c1b201b0022e161a0e2a8d0dbf83720c2b448ac
Hikaru Ohmori <hikaru@oomorihikari-no-MacBook-Air.local>
2016-05-12 15:06:30 +0900

    add3

---
+++ hikaru/chapter03/knock28.py
@@ -0,0 +1,31 @@
+#-*-coding: utf-8-*-
+import json, re
+f = open('jawiki-country.json','r')
+for line in f:
+    jsonData = json.loads(line)
+    if jsonData['title'] == "イギリス":
+        break 
+
+UK_text = jsonData['text'].strip() 
+find_list = re.findall('==+(.*?)==+', UK_text)# + 直前の正規表現の1回以上の繰り返し
+#print (find_list)
+
+UK_dict = {}
+UK_dict2 = {}
+UK_list = jsonData['text'].splitlines()
+for (i, line) in enumerate(UK_list):
+    if line.count('='):
+        field = re.search('\|(.*?)=', line)
+        value = re.search('=(.*)', line)
+        if field != None and value != None:
+            UK_dict[field.group(1).strip()] = value.group(1).strip()
+    if line == '}}':
+        break
+
+for fi, va in sorted(UK_dict.items()):
+    va = va.replace("'''", '')
+    va = re.sub('\[\[|\]\]', '', va)
+    va = re.sub('<.*ref.*?>', '', va)
+    va = re.sub('<br.*>', '', va)
+    print ("field:{} value:{}".format(fi, va))
+#print (UK_dict.items())
