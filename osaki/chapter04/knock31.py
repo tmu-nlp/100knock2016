1668c18685dd16cafe6faa47da5ff2bc5c692366
osaki <osaki@osaki.local>
2016-05-13 13:28:05 +0900

    add

---
+++ osaki/chapter04/knock31.py
@@ -0,0 +1,20 @@
+#-*- coding:utf-8 -*-
+import re
+from collections import defaultdict
+d=defaultdict(lambda:0)
+l=list()
+
+for line in open("neko.txt.mecab","r"):
+    if line == "EOS\n":
+        continue
+    line=re.sub(",,*",",",line.replace("\t"," ").replace(" ",","))
+    d={}
+    d["表層形"]=line.split(",")[0]
+    d["基本形"]=line.split(",")[-3]
+    d["品詞"]=line.split(",")[1]
+    d["品詞細分類１"]=line.split(",")[2]
+    l.append(d)
+
+for words in l:
+    if words["品詞"]=="動詞":
+        print(words["表層形"])
