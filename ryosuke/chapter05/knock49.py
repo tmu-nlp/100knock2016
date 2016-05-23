9a7be90fd126a662ce8e66330b62700952260257
ryosukee <tmcit.miyazaki@gmail.com>
2016-05-24 05:29:18 +0900

    [modify] minor modified

--- ryosuke/chapter05/knock49.py
+++ ryosuke/chapter05/knock49.py
@@ -6,7 +6,6 @@ def get_path(n1, n2, sentence):
     if n2.id < n1.id:
         n1, n2 = n2, n1
     n1path = n1.get_path_to_root(sentence, form=lambda ch: ch.id)
-    n2path = n2.get_path_to_root(sentence, form=lambda ch: ch.id)
     # same path
     if n2.id in n1path:
         def get_x2y(ch):
@@ -24,6 +23,7 @@ def get_path(n1, n2, sentence):
                 return ch.replace_noun(XorY)
             return ch.join_surface_wo_symbol()
         return replace
+    n2path = n2.get_path_to_root(sentence, form=lambda ch: ch.id)
     first_shared_id = min(set(n1path) & set(n2path))
     n1path = '->'.join(n1.get_path_to_id(sentence, first_shared_id, form=get_replaced('X', n1.id), include_id=False))
     n2path = '->'.join(n2.get_path_to_id(sentence, first_shared_id, form=get_replaced('Y', n2.id), include_id=False))
