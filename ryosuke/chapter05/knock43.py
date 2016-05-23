e0a4bfc8c30015241cd5390cd50fba19475ceb62
ryosukee <tmcit.miyazaki@gmail.com>
2016-05-23 17:32:03 +0900

    [add] initial commit

---
+++ ryosuke/chapter05/knock43.py
@@ -0,0 +1,13 @@
+from knock41 import get_sentences
+
+for sentence in get_sentences():
+    for chunk in sentence:
+        if chunk.dst != -1:
+            tgt_chunk = sentence[chunk.dst]
+            if not chunk.in_noun() or not tgt_chunk.in_verb():
+                continue
+            src = chunk.join_surface_wo_symbol()
+            dst = tgt_chunk.join_surface_wo_symbol()
+            if src == '' or dst == '':
+                continue
+            print('{}\t{}'.format(src, dst))
