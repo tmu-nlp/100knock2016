7554fbb91b17d5f6cd890e9b8b0a1c072e064948
osaki <osaki@osaki.local>
2016-05-25 00:51:02 +0900

    modified

---
+++ osaki/chapter05/knock42.py
@@ -0,0 +1,20 @@
+#-*- coding:utf-8 -*-
+from knock40 import mkMorphclass
+from knock41 import mkChunkclass
+
+s=""
+t=""
+for line in mkChunkclass(mkMorphclass()):
+    for phrase in line:
+        d=int(phrase.dst)
+        for word in phrase.morph:
+            if word.pos!=u"記号":
+                s+=word.surface
+        for phrase_d in line:
+            if int(phrase_d.srcs)==d:
+                for word in phrase_d.morph:
+                    if word.pos!=u"記号":
+                         t+=word.surface
+        print(s+"\t"+t)
+        s=""
+        t=""
