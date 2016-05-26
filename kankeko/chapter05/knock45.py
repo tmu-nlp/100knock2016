e24b853fd1e31cd605c8bad46ec9114a6b816ca8
正弘金子 <masahirokaneko@masahirokaneko-no-MacBook-Air.local>
2016-05-26 17:24:50 +0900

    44出来なかった(´・ω・｀)

--- kankeko/chapter05/knock45.py
+++ kankeko/chapter05/knock45.py
@@ -1,18 +1,18 @@
 from knock41 import get_cabocha
 verb = []
 mylist = []
-for sentence in get_cabocha():
-    for chunk in sentence:
+for s in get_cabocha():
+    for chunk in s:
         if any(m.pos == "動詞" for m in chunk.morphs):
             mylist = []
-            for src_num in chunk.srcs:
-                src_chunk = sentence[src_num]
+            for src in chunk.srcs:
+                src_chunk = s[src]
                 if any(m.pos == "助詞" for m in chunk.morphs):
                     line = [m.surface for m in chunk.morphs if m.pos == '助詞'][0]
-                if line is not None:
+                if line != "":
                     mylist.append(line)
         if len(mylist) != 0:
             if any(m.pos == "動詞" for m in chunk.morphs):
-                verb = [m.base for m in chunk.morphs if m.pos == '動詞'][0]
-                join_line = ' '.join(sorted(set(mylist)))
-                print('{}\t{}'.format(verb, join_line))
+                mylist = set(mylist)
+                join_line = ' '.join(mylist)
+                print('{}\t{}'.format([m.base for m in chunk.morphs if m.pos == '動詞'][0], join_line))
