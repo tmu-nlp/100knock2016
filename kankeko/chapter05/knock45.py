1af546e7da6acd8e2e92d0eeaa8dffd77d631137
正弘金子 <masahirokaneko@masahirokaneko-no-MacBook-Air.local>
2016-05-26 17:39:52 +0900

    44出来なかった(´・ω・｀)

--- kankeko/chapter05/knock45.py
+++ kankeko/chapter05/knock45.py
@@ -10,8 +10,8 @@ for s in get_cabocha():
                 if any(m.pos == "助詞" for m in chunk.morphs):
                     line = str([m.surface for m in chunk.morphs if m.pos == '助詞'])
                     mylist.append(line)
-        if len(mylist) != 0:
-            if any(m.pos == "動詞" for m in chunk.morphs):
-                mylist = set(mylist)
-                join_line = ' '.join(mylist)
-                print('{}\t{}'.format(str([m.base for m in chunk.morphs if m.pos == '動詞']), join_line))
+        #if len(mylist) != 0:
+        if any(m.pos == "動詞" for m in chunk.morphs):
+            mylist = set(mylist)
+            join_line = ' '.join(mylist)
+            print('{}\t{}'.format(str([m.base for m in chunk.morphs if m.pos == '動詞']), join_line))
