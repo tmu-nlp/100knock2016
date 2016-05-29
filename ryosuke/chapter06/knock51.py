26c911f7b13311f568ab2c5c52022f910916977d
ryosukee <tmcit.miyazaki@gmail.com>
2016-05-30 06:27:15 +0900

    [modify] remove punctuation

--- ryosuke/chapter06/knock51.py
+++ ryosuke/chapter06/knock51.py
@@ -1,4 +1,11 @@
+def remove_punctuation(s):
+    for punc in ['.', ',', ';', ':', '?', '!']:
+        if s.endswith(punc):
+            return s[:-1]
+    return s
+
 for line in open('knock50.txt'):
-    spl = line.split(' ')
+    spl = line.rstrip('\n').split(' ')
     for tok in spl:
-        print(tok)
+        print(remove_punctuation(tok))
+    print()
