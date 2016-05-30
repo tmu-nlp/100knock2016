4c88daef9604b9d5b3a3f531e9fc7a7b843a19f0
ryosukee <tmcit.miyazaki@gmail.com>
2016-05-31 04:21:15 +0900

    [modify] delete remove_punctuation function

--- ryosuke/chapter06/knock51.py
+++ ryosuke/chapter06/knock51.py
@@ -1,11 +1,4 @@
-def remove_punctuation(s):
-    for punc in ['.', ',', ';', ':', '?', '!']:
-        if s.endswith(punc):
-            return s[:-1]
-    return s
-
 for line in open('knock50.txt'):
-    spl = line.rstrip('\n').split(' ')
+    spl = line.split(' ')
     for tok in spl:
-        print(remove_punctuation(tok))
-    print()
+        print(tok)
