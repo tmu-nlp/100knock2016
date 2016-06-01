fd3f4a728d36a7b3337f7916302d91e2cc92cc5d
yui8e <s.yui1122@gmail.com>
2016-06-01 23:59:06 +0900

    add 05

---
+++ yui/chapter05/knock43.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+#43.
+
+from knock41 import get_sentences
+
+
+for sentence in get_sentences():
+    for chunk in sentence:
+        if chunk.dst != -1:
+            tgt_chunk = sentence[chunk.dst]
+            if not chunk.has_noun() or not tgt_chunk.has_verb():
+                continue
+            src = chunk.join_surface_wo_symbol()
+            dst = tgt_chunk.join_surface_wo_symbol()
+            if src == " or dst == ":            #??
+                continue
+            print('{}\t{}'.format(src, dst))    #??
---
+++ yui/chapter05/knock46.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+
+
+from knock41 import get_sentences
+
+
+for sentence in get_sentences():
+    for chunk in sentence:
+        if chunk.has_verb():
+            candidate = list()
+            for src_id in chunk.srcs:
+                src_chunk = sentence[src_id]
+                part = src_chunk.get_most_right_particle()
+                if part is not None:
+                    candidate.append((part, src_chunk.join_surface()))
+            if len(candidate) != 0:
+                verb = chunk.get_most_left_verb()
+                particles = ' '.join(part for part, ch in sorted(set(candidate)))
+                chunks = ' '.join(ch for part, ch in sorted(set(candidate)))
+                print('{}\t{}\t{}'.format(verb, particles, chunks))
