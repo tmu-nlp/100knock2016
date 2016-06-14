680b38f2fd5e8de0856bde2a53835456b831e170
尾形朋哉 <tomoya@tmy.local>
2016-06-14 09:26:59 +0900

    upgrade

--- tomoya/chapter07/knock69.py
+++ tomoya/chapter07/knock69.py
@@ -3,28 +3,26 @@
 import cgi
 from pymongo import MongoClient
 
-html2 = '''Content-Type: text/html
-<html>
-<head>
-  <title>アーティスト検索</title>
-  <style type="text/css">
-<!--
-pre {
-  border-width: 1px;
-  border-style: dotted;
-  border-color: #009999;
-  margin: 1em;
-  padding: 1em;
-}
--->
-  </style>
-</head>
-<body>
-<h1>結果</h1>
-<pre>%s</pre>
-</body>
-</html>
-'''
+print("Content-Type: text/html\r\n\r\n")
+print("<html>")
+print("<head>")
+print("<title>検索結果</title>")
+print("<style>")
+print("table{border-collapse:collapse;}")
+print("td{border:solid 1px;\npadding: 0.5em;}")
+print("</style>")
+print("</head>")
+print("<body>")
+print("<table>")
+print("<caption>検索結果</caption>")
+print("<tr>")
+print("<td>アーティスト名</td>")
+print("<td>別名</td>")
+print("<td>タグ</td>")
+print("<td>活動場所</td>")
+#print("<td>活動開始</td>")
+#print("<td>活動終了</td>")
+print("</tr>")
 
 f = cgi.FieldStorage()
 attributes = ['name', 'aliases.name', 'tags.value', 'area']
@@ -38,10 +36,21 @@ find_dict = dict()
 for attr, txt in zip(attributes, txt):
     if txt != '':
         find_dict[attr] = txt
-with open('output.txt', 'w') as fp:
-    for data in collection.find(find_dict, {'gid': 0, 'id': 0, '_id': 0}).sort([('rating.count', -1)]):
-        for key, value in sorted(data.items()):
-            print('{}\t{}'.format(key, value), file=fp)
-        print(file=fp)
-fp = open('output.txt', 'r').read()
-print(html2 % cgi.escape(fp))
+data_list = ['name', 'aliases', 'tags', 'area']
+data_dict = {'aliases': 'name', 'tags': 'value'}
+for data in collection.find(find_dict, {'gid': 0, 'id': 0, '_id': 0}).sort([('rating.count', -1)]):
+    print("<tr>")
+    for x in data_list:
+        if x in data_dict.keys() and x in data.keys():
+            print("<td>")
+            for y in data[x]:
+                print("{} ".format(y[data_dict[x]]))
+            print("</td>")
+        elif x not in data.keys():
+            print("<td></td>")
+        else:
+            print("<td>{}</td>".format(data[x]))
+    print("</tr>")
+
+print("</body>")
+print("</html>")
