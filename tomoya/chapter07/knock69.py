6dcc836241c017c75f6ca90a1968fc145410409d
尾形朋哉 <tomoya@tmy.local>
2016-06-15 17:27:15 +0900

    upgrade

--- tomoya/chapter07/knock69.py
+++ tomoya/chapter07/knock69.py
@@ -8,11 +8,18 @@ print("<html>")
 print("<head>")
 print("<title>検索結果</title>")
 print("<style>")
-print("table{border-collapse:collapse;}")
-print("td{border:solid 1px;\npadding: 0.5em;}")
+print("table{border-collapse:collapse;\ntable-layout: fixed;}")
+print("td{border:solid 1px;\npadding: 2em;}")
 print("</style>")
 print("</head>")
 print("<body>")
+print('<form action="knock69.py" method="post">')
+print('<p>アーティスト名<input type="text" name="name"></p>')
+print('<p>別名<input type="text" name="aliases.name"></p>')
+print('<p>タグ<input type="text" name="tags.value"></p>')
+print('<p>活動場所<input type="text" name="area"></p>')
+print('<p><input type="submit" value="検索"></p>')
+print("</form>")
 print("<table>")
 print("<caption>検索結果</caption>")
 print("<tr>")
@@ -20,8 +27,8 @@ print("<td>アーティスト名</td>")
 print("<td>別名</td>")
 print("<td>タグ</td>")
 print("<td>活動場所</td>")
-#print("<td>活動開始</td>")
-#print("<td>活動終了</td>")
+print("<td>活動開始</td>")
+print("<td>活動終了</td>")
 print("</tr>")
 
 f = cgi.FieldStorage()
@@ -36,8 +43,9 @@ find_dict = dict()
 for attr, txt in zip(attributes, txt):
     if txt != '':
         find_dict[attr] = txt
-data_list = ['name', 'aliases', 'tags', 'area']
+data_list = ['name', 'aliases', 'tags', 'area', 'begin', 'end']
 data_dict = {'aliases': 'name', 'tags': 'value'}
+time = ['年', '月', '日']
 for data in collection.find(find_dict, {'gid': 0, 'id': 0, '_id': 0}).sort([('rating.count', -1)]):
     print("<tr>")
     for x in data_list:
@@ -48,9 +56,14 @@ for data in collection.find(find_dict, {'gid': 0, 'id': 0, '_id': 0}).sort([('ra
             print("</td>")
         elif x not in data.keys():
             print("<td></td>")
+        elif x in ['begin', 'end']:
+            a = zip(map(str, sorted(data[x].values(), reverse=True)), time)
+            print("<td>")
+            for t in a:
+                print("".join(t), end="")
+            print("</td>")
         else:
             print("<td>{}</td>".format(data[x]))
     print("</tr>")
-
 print("</body>")
 print("</html>")
