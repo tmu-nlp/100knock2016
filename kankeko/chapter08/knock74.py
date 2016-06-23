33340ac691623752157b213745cee319ab32d9c6
正弘金子 <masahirokaneko@masahirokaneko-no-MacBook-Air.local>
2016-06-23 16:47:59 +0900

    ttt

--- kankeko/chapter08/knock74.py
+++ kankeko/chapter08/knock74.py
@@ -14,9 +14,9 @@ def predict_function():
     pred = predict_doc.predict(X)
     count = 0
     total = 0
-    for p, y in zip(pred, y_train):
-        print(p, y)
-        if int(p) == y:
+    for a, y in zip(pred, y_train):
+        print(a, y)
+        if int(a) == y:
             count += 1
         total += 1
     print(count/total)
