67abf9fa1e4a814ab682d9949966b693e2479706
ryosukee <tmcit.miyazaki@gmail.com>
2016-07-07 07:55:59 +0900

    [fix] output proba

--- ryosuke/chapter08/knock74.py
+++ ryosuke/chapter08/knock74.py
@@ -6,12 +6,16 @@ clf = joblib.load('knock73.model')
 dic2vec = DictVectorizer()
 features = list()
 y = list()
+sents = list()
 for line in open('sentiment.txt'):
     spl = line.rstrip('\n').split()
     label, sent = spl[0], ' '.join(spl[1:])
+    sents.append(sent)
     features.append(get_feature(sent))
     y.append(int(label))
 x = dic2vec.fit_transform(features)
 pred = clf.predict(x)
+pred_proba = clf.predict_proba(x)
 
-print('{}%'.format(sum(p == y for p, y in zip(pred, y)) / len(pred)))
+for pre, proba, sent in zip(pred, pred_proba, sents):
+    print(pre, proba, sent)
