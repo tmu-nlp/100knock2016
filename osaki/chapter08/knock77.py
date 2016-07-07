25b9ad0e13d9f8f27ce64f708ad512d767828540
osaki <osaki@osaki.local>
2016-07-07 15:06:18 +0900

    modified

--- osaki/chapter08/knock77.py
+++ osaki/chapter08/knock77.py
@@ -1,14 +1,14 @@
 import sys
 from collections import defaultdict
-from knock76 import mk_label
 
-def acc_(f):
+def acc_(f,d,thre):
+    from knock76 import mk_label
     tp=0
     tn=0
     fp=0
     fn=0
-    for line in f:
-        item=mk_label(line).split("\t")
+    for line in f:#.split("\n"):
+        item=mk_label(line,d,thre).split("\t")
         if item[0]=="+1" and item[1]=="+1":
             tp+=1
         elif item[0]=="-1" and item[1]=="-1":
@@ -24,4 +24,8 @@ def acc_(f):
     return(acc,pre,rec,F)
 
 if __name__=="__main__":
-    print(acc_(open(sys.argv[2])))
+    d=defaultdict(lambda:0)
+    for line_d in open("model.txt","r"):
+        data=line_d.strip("\n").split("\t")
+        d[data[0]]=float(data[1])
+    print(acc_(open(sys.argv[1]),d,0.5))
