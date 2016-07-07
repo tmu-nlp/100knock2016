9a03027aaa0097b4f3e1cff70b3cf645992adb28
osaki <osaki@osaki.local>
2016-07-07 17:23:38 +0900

    modified

--- osaki/chapter08/knock79.py
+++ osaki/chapter08/knock79.py
@@ -5,14 +5,11 @@ from knock77 import acc_
 
 pre=list()
 rec=list()
-thre=list()
 d=rog_learn(10,open(sys.argv[1]))
 for i in range(50):
     result=acc_(open(sys.argv[1]),d,i/100)
     pre+=[result[1]]
     rec+=[result[2]]
-    thre+=[i]
 
-print(result)
 plt.plot(pre,rec,color="red")
 plt.show()
