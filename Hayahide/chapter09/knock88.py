d5a98d07fa8b5eea2392997757c9002feed1234d
山岸駿秀 <hargon@Hayahide-Mac.local>
2016-07-14 12:51:57 +0900

    modified 7/14

--- Hayahide/chapter09/knock88.py
+++ Hayahide/chapter09/knock88.py
@@ -6,7 +6,7 @@ with open('knock85_result.dump', 'rb') as f:
 
 similarity = dict()
 for word, vector in word_matrix.items():
-    similarity[word] = dis.cosine(word_matrix['England'], word_matrix[word])
+    similarity[word] = 1 - dis.cosine(word_matrix['England'], word_matrix[word])
 
-for word, similar in sorted(similarity.items(), key=lambda x:x[1], reverse=True)[:10]:
+for word, similar in sorted(similarity.items(), key=lambda x:x[1], reverse=True)[1:11]:
     print(word + '\t' + str(similar))
--- Hayahide/chapter09/knock89.py
+++ Hayahide/chapter09/knock89.py
@@ -7,7 +7,7 @@ with open('knock85_result.dump', 'rb') as f:
 similarity = dict()
 cvec = word_matrix['Spain'] - word_matrix['Madrid'] + word_matrix['Athens']
 for word, vector in word_matrix.items():
-    similarity[word] = dis.cosine(cvec, word_matrix[word])
+    similarity[word] = 1 - dis.cosine(cvec, word_matrix[word])
 
 for word, similar in sorted(similarity.items(), key=lambda x:x[1], reverse=True)[:10]:
     print(word + '\t' + str(similar))
