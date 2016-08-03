de094450b5eb65d4db94e0f87fef2d7a27477e37
Yukio Matsumura <Yukio@Yukio-no-MacBook-Air.local>
2016-08-03 10:49:30 +0900

    updated

--- yukio/chapter10/knock99.py
+++ yukio/chapter10/knock99.py
@@ -11,5 +11,8 @@ for country, vector in sorted(extract_countries().items()):
 model = TSNE()
 new_vectors = model.fit_transform(vectors)
 
-plt.scatter([vec[0] for vec in new_vectors], [vec[1] for vec in new_vectors]) 
+fig, ax = plt.subplots()
+ax.scatter(new_vectors[:, 0], new_vectors[:, 1])
+for index, label in enumerate(countries):
+    ax.annotate(label, xy=(new_vectors[index, 0], new_vectors[index, 1]))
 plt.show()
